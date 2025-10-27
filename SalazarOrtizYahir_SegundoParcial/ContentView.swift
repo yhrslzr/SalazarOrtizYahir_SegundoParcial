8a4e3a1f5b9ad4a7403c36b3b04d8bfed82d30b3
yhrslzr <yahir.salazarortiz0113@gmail.com>
2025-10-27 17:35:04 -0600

    Initial Commit

---
+++ SalazarOrtizYahir_SegundoParcial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SalazarOrtizYahir_SegundoParcial
+//
+//  Created by Yahir Salazar on 27/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SalazarOrtizYahir_SegundoParcial/SalazarOrtizYahir_SegundoParcialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalazarOrtizYahir_SegundoParcialApp.swift
+//  SalazarOrtizYahir_SegundoParcial
+//
+//  Created by Yahir Salazar on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SalazarOrtizYahir_SegundoParcialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
